11d5234c49855ec24c16b66cffbc6102a7830a84
Tony Stone <hatter24@gmail.com>
2016-10-14 06:01:02 -0700

    Adding test project and cocoapod.

---
+++ Example/Pods/Target Support Files/Pods-TravisSandbox_Tests/Pods-TravisSandbox_Tests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_TravisSandbox_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TravisSandbox_TestsVersionString[];
+
---
+++ Example/Pods/Target Support Files/TravisSandbox/TravisSandbox-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TravisSandbox : NSObject
+@end
+@implementation PodsDummy_TravisSandbox
+@end
